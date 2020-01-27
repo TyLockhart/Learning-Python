c35cb3c1d10766cc5bba6e5624f333968d6fde9c
TyLockhart <53748468+TyLockhart@users.noreply.github.com>
2020-01-27 18:55:52 -0500

    Jan27, 2020

---
+++ app.py
@@ -0,0 +1,111 @@
+"""
+print("Ty Lockhart")
+print("o----")
+print(" ||||")
+print("*" * 10)
+
+
+
+price = 10
+price = 20
+rating = 4.9
+name2 = "Ty"
+is_published = True
+print(name2)
+
+
+name3 = "John Smith"
+age = 20
+is_new = False
+
+print("*"*20)
+
+name = input("What is your name? ")
+print("Hi "+ name)
+
+print("*"*20)
+
+name_question = input("What is your name? ")
+colour_question = input("What is your favourite colour? ")
+print(name_question + " likes " + colour_question)
+
+
+birth_year = input("Birth year: ")
+print(type(birth_year))
+age = 2019-int(birth_year)
+#2019 - "1982"
+
+print(type(age))
+print(age)
+
+lb_weight=input("What is your weight (in lbs)? ")
+kg_weight=0.453592*float(lb_weight)
+print("You weigh (in kg) "+ str(kg_weight))
+
+
+
+course = "Python for Beginners"
+course2='''
+Hi John,
+
+Here is our first email to you.
+
+Thank you,
+The support team
+
+'''
+course3="Python for Beginners"
+print(course3[0:3])
+another = course3[:]
+print(another)
+
+"""
+
+name = "Jennifer"
+print(name[1:-1])
+#guessing it will print e and r // Print ennife
+
+first = "John"
+last = "Smith"
+message=first+" [" + last + "] is a coder"
+msg= f"{first} [{last}] is a coder" #formatted strings
+print(message)
+
+#stopped at 40:00 https://www.youtube.com/watch?v=_uQrJ0TkZlc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
